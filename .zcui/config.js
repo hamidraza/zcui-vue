18517d271b882d732dbcfd50dece5f6462589db6
Hamid Raza <contact@hamidraza.net>
2018-07-21 09:38:58 +0530

    feat: adds .zcui

---
+++ .zcui/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  variant: 'vue',
+  path: {
+    components: 'src/components',
+    pages: 'src/pages',
+    layouts: 'src/layouts',
+    store: 'src/store',
+    style: 'src/styles',
+  },
+};
+
